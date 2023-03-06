7f4f984298b211f177578a1da19a71b1cf9d8bcc
adam.mach <adam.mach@VT1-15>
2023-03-06 10:42:05 +0100

    Přidejte soubory projektu.

---
+++ Hellogithub/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Ahoj,Já jsem Adam Mach , chodím na SŠ Educhem a mam rád :");
+
+string[] zajmy = { "formule 1", "své kamarády", "klid", "filmy a seriály ",   };
+foreach (var item in zajmy )
+{
+    Console.WriteLine(item);
+}
